bc6ff5112ab740e7742397ab8f3433d3688b0d23
Donghyun Gouk <kukdh1@gmail.com>
2022-05-08 14:48:11 +0900

    Add logics...

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as which from 'which';
+import * as child_process from 'child_process'
 
 async function findFormatter(path: string): Promise<string | undefined> {
   try {
@@ -16,10 +17,97 @@ async function runFormatter(
   range?: vscode.Range,
   token?: vscode.CancellationToken
 ): Promise<vscode.TextEdit[]> {
-  // TODO: invoke formatter
+  // Read configuration
+  let config = vscode.workspace.getConfiguration('verible-formatter')
+  let path: string | undefined = config.get('path')
+  let flagfile: string | undefined = config.get('flagFile')
 
+  // Check path
+  if (!path || path.trim().length == 0) {
+    vscode.window.showErrorMessage('Path to verible-verilog-format not specified.')
+
+    return []
+  }
+
+  path = await findFormatter(path)
+
+  if (!path) {
+    return []
+  }
+
+  // Find flagfile
+  if (flagfile) {
+    let uri = vscode.Uri.file(flagfile)
+
+    try {
+      await vscode.workspace.fs.stat(uri)
+    }
+    catch (e) {
+      flagfile = undefined
+    }
+  }
+
+  // Make options
+  let params: Array<string> = []
+
+  if (flagfile) {
+    params.push('--flagfile=' + flagfile)
+  }
+
+  if (range) {
+    params.push(`--lines=${range.start.line + 1}-${range.end.line + 1}`)
+  }
+
+  // Contents will be fed from stdin
+  params.push('-')
+
+  // Invoke formatter
+  let proc = child_process.spawn(path, params, {
+    env: process.env,
+    stdio: 'pipe'
+  })
+
+  // Encoding
+  proc.stdout.setEncoding('utf-8')
+  proc.stderr.setEncoding('utf-8')
+
+  // Fed input
+  await new Promise<void>((resolve, reject) => {
+    proc.stdin.end(document.getText(), 'utf-8', () => {
+      resolve()
+    })
+  })
+
+  // Get output/error
+  let out: Array<string> = []
+  let err: Array<string> = []
+
+  for await (let chunk of proc.stdout) {
+    out.push(chunk)
+  }
+
+  for await (let chunk of proc.stderr) {
+    err.push(chunk)
+  }
+
+  // Canceled?
   if (token?.isCancellationRequested) {
     return []
+  }
+
+  if (proc.exitCode && proc.exitCode == 0) {
+    return [
+      vscode.TextEdit.replace(
+        new vscode.Range(
+          document.lineAt(0).range.start,
+          document.lineAt(document.lineCount - 1).range.end
+        ),
+        out.join()
+      )
+    ]
+  }
+  else {
+    vscode.window.showErrorMessage('Formatting failed with error:\n' + err.join())
   }
 
   return [] // TODO: fill this
