57f889b3069bc64d4580fae4f08e40fa46ab1c1f
Donghyun Gouk <kukdh1@gmail.com>
2022-05-08 13:03:23 +0900

    Fill formatter installation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,43 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "verible-formatter" is now active!');
+async function runFormatter(
+  document: vscode.TextDocument,
+  range?: vscode.Range,
+  token?: vscode.CancellationToken
+): Promise<vscode.TextEdit[]> {
+  // TODO: invoke formatter
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('verible-formatter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Verible Formatter!');
-	});
+  if (token?.isCancellationRequested) {
+    return []
+  }
 
-	context.subscriptions.push(disposable);
+  return [] // TODO: fill this
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+  let _formatter = vscode.languages.registerDocumentFormattingEditProvider(
+    ['verilog', 'systemverilog'], {
+    provideDocumentFormattingEdits(
+      document: vscode.TextDocument
+    ): vscode.ProviderResult<vscode.TextEdit[]> {
+      return runFormatter(document)
+    }
+  })
+
+  let _formatter_range = vscode.languages.registerDocumentRangeFormattingEditProvider(
+    ['verilog', 'systemverilog'], {
+    provideDocumentRangeFormattingEdits(
+      document: vscode.TextDocument,
+      range: vscode.Range,
+      options: vscode.FormattingOptions,
+      token: vscode.CancellationToken
+    ): vscode.ProviderResult<vscode.TextEdit[]> {
+      return runFormatter(document, range, token)
+    }
+  })
+
+  context.subscriptions.push(_formatter)
+  context.subscriptions.push(_formatter_range)
+}
+
+export function deactivate() { }
